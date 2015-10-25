54d2998ac8a39fb179c13cd3d1d9e75b57d14529
Samuele Resca <samuele.resca@gmail.com>
2015-10-25 19:47:05 +0100

    Added dialogManager.js

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,12 @@
 //modules inclusion
 require('./scripts/infoLog');
 require('./scripts/fileManager');
+require('./scripts/dialogManager');
 
 //On load
 window.onload = function () {
 
   $M.infoLog();
   $M.fileManager();
+  $M.dialogManager();
 }
