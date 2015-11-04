85d83a567f18f2de57985edcfbe7e504ee0cac1e
Samuele Resca <samuele.resca@gmail.com>
2015-11-04 13:57:08 +0100

    File reading

--- scripts/init.js
+++ scripts/init.js
@@ -3,9 +3,16 @@ require('./scripts/infoLog');
 require('./scripts/fileManager');
 require('./scripts/dialogManager');
 
+var fs = require('fs')
+
 //On load
 window.onload = function () {
   $M.infoLog();
+  
+  fs.readFile('scripts/test.txt', 'utf8', function(err,data) {
+    console.log(data);
+  });
+  
   $M.fileManager();
   $M.dialogManager();
 }
